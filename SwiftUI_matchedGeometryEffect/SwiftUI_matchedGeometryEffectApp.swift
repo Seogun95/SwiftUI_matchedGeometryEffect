458bd6249836d2f28fc8ea58ac9c2aba152f9f44
SeonJungKim <ksj083895@gmail.com>
2021-04-30 11:10:29 +0500

    Initial Commit

---
+++ SwiftUI_matchedGeometryEffect/SwiftUI_matchedGeometryEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_matchedGeometryEffectApp.swift
+//  SwiftUI_matchedGeometryEffect
+//
+//  Created by 김선중 on 2021/04/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_matchedGeometryEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
